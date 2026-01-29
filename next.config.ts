0c2f58320b31172d5365bbf5d449975fb97fba55
nelaka wedagama <nelakawedagama@gmail.com>
2026-01-29 16:04:28 +0000

    Error: ./node_modules/@opentelemetry/context-async-hooks/build/src/Async

--- next.config.ts
+++ next.config.ts
@@ -38,7 +38,7 @@ const nextConfig: NextConfig = {
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
-        ...config.resolve.fallback,
+        ...(config.resolve.fallback || {}),
         async_hooks: false,
       };
     }
